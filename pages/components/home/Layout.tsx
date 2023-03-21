733f4fcac74bb07d8d42e680d14b94abbf39490f
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-03-21 21:40:23 +0900

    initioal commit

---
+++ pages/components/home/Layout.tsx
@@ -0,0 +1,215 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { ReactNode } from "react";
+import Tasky_weblogo from "public/Tasky_header.svg";
+import Link from "next/link";
+
+const Layout = ({ children, title }: Props) => {
+  return (
+    <>
+      {/* メタ情報 ------------ */}
+      <Head>
+        <title>{title}</title>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <main className="h-screen flex flex-col">
+        {/* トップ バー ------------ */}
+        <header className="
+         top-0 left-0 right-0 z-50 m-0 py-3 h-20 bg-white shadow-md ">
+          <nav className="flex justify-between items-center mx-auto">
+            <div className="pl-4">
+              <Link href="/" passHref={true}>
+                <Image
+                  src={Tasky_weblogo}
+                  className="w-28 h-auto"
+                  alt={"TaskyWebLogo"}
+                />
+              </Link>
+            </div>
+            <div className="flex pr-8 space-x-12 font-bold">
+              <Link href="/" passHref={true}>
+                <span className="text-xl transition-all duration-200 hover:text-blue-500 cursor-pointer">
+                  ダッシュボード
+                </span>
+              </Link>
+              <Link href="/" passHref={true}>
+                <span className="text-xl transition-all duration-200 hover:text-blue-500 cursor-pointer">
+                  タスク
+                </span>
+              </Link>
+              <Link href="/" passHref={true}>
+                <span className="text-xl transition-all duration-200 hover:text-blue-500 cursor-pointer">
+                  メンバー
+                </span>
+              </Link>
+              <Link href="/" passHref={true}>
+                <span className="text-xl transition-all duration-200 hover:text-blue-500 cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={3}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                    />
+                  </svg>
+                </span>
+              </Link>
+              <Link href="/" passHref={true}>
+                <span className="text-xl transition-all duration-200 hover:text-blue-500 cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={3}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                    />
+                  </svg>
+                </span>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <hr />
+
+        {/* サイド バー ------------ */}
+        <div className="flex flex-grow">
+          <div className="w-1/6 bg-blue-900 text-center text-white">
+            <Link href="/">
+              <span className="flex font-bold text-xl py-6 pl-8 transition duration-200 hover:bg-white hover:text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </svg>
+                <p className="px-6">ファイル</p>
+              </span>
+            </Link>
+            <Link href="/about">
+              <span className="flex font-bold text-xl py-6 pl-8 transition duration-200 hover:bg-white hover:text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                  />
+                </svg>
+                <p className="px-6">カレンダー</p>
+              </span>
+            </Link>
+            <Link href="/blog">
+              <span className="flex font-bold text-xl py-6 pl-8 transition duration-200 hover:bg-white hover:text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                  />
+                </svg>
+                <p className="px-6">メッセージ</p>
+              </span>
+            </Link>
+            <Link href="/contact">
+              <span className="flex font-bold text-xl py-6 pl-8 transition duration-200 hover:bg-white hover:text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+                  />
+                </svg>
+                <p className="px-6">ガントチャート</p>
+              </span>
+            </Link>
+            <Link href="/faq">
+              <span className="flex font-bold text-xl py-6 pl-8 transition duration-200 hover:bg-white hover:text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+                <p className="px-6">設定</p>
+              </span>
+            </Link>
+            <div className="fixed bottom-0 left-0 p-10 m-8 rounded-2xl bg-white text-sm">
+              <p className="py-5 text-blue-500 text-center  text-xl font-bold">
+                Manager
+              </p>
+              <p className="py-5 text-blue-500 text-center  text-xl font-bold">
+                Username
+              </p>
+            </div>
+          </div>
+          {/* メインコンテンツ ------------ */}
+          <div className="w-5/6">{children}</div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+interface Props {
+  children: ReactNode;
+  title: string;
+}
+
+export default Layout;
